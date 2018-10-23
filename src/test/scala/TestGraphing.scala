9cdc088ecce3ab42bfe21bff4d4396126699c44e
funwarioisii <g231q026@s.iwate-pu.ac.jp>
2018-10-23 10:11:29 +0900

    ok

--- src/test/scala/TestGraphing.scala
+++ src/test/scala/TestGraphing.scala
@@ -2,11 +2,11 @@ import org.scalatest._
 
 
 class TestGraphing extends FlatSpec with DiagrammedAssertions{
-  val graphing = Graphing
+  private val graphing = Graphing
 
 
   "getGraphData" should "グラフからほしいデータを取得できる" in {
-    val mess = graphing.getGraphData(310L, 320L)
+    val mess:String = graphing.getGraphData(src = 310L, dst = 320L)._1
 
     assert(mess === "310_to_320")
   }
@@ -19,5 +19,10 @@ class TestGraphing extends FlatSpec with DiagrammedAssertions{
     assert(graphing.getCallableNodes(310L).contains(320L))
     assert(graphing.getCallableNodes(310L).contains(340L))
     assert(!graphing.getCallableNodes(310L).contains(350L))  // 350にはつながってないはず
+  }
+
+  "updateProb" should "EdgeのProbを変えられる" in {
+    graphing.updateProb(310L, 320L, 0.8F)
+    assert(graphing.getGraphData(src = 310L, dst = 320L)._2 === 0.8f)
   }
 }
